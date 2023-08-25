1dec4c0188fede591a6ab62212c5124ea1bad076
TH3Mateo <th3mateo@gmail.com>
2023-08-26 00:27:06 +0200

    done some test tool

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 
 import importlib.util as ilu
-spec1 = ilu.spec_from_file_location("coms",r"C:\\Users\\M\Desktop\\python_code\\softHeater\\connection_module.py")
+import time
+spec1 = ilu.spec_from_file_location("coms",r"D:\PycharmProjects\heater_gui\connection_module.py")
 coms = ilu.module_from_spec(spec1)
 spec1.loader.exec_module(coms)
-spec2 = ilu.spec_from_file_location("hal",r"C:\\Users\\M\Desktop\\python_code\\softHeater\\dev_spec_layer.py")
+spec2 = ilu.spec_from_file_location("hal",r"D:\PycharmProjects\heater_gui\dev_spec_layer.py")
 hal = ilu.module_from_spec(spec2)
 spec2.loader.exec_module(hal)
 
+msg = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,0x00,0x00]
+
 def main():
+    print("main")
     usb = coms.USB_device()
     usb.start()
+    print("init done")
+    usb.transmit_queue.put(msg)
     while True:
         while usb.receive_queue.qsize() > 0:
             print(usb.receive_queue.get())
-        
         x = input("enter command: ")
-        usb.transmit_queue.put(x)
-
+        if len(x) ==2:
+            msg[0] = int(x[0])
+            msg[-1] = int(x[1])
+            usb.transmit_queue.put(msg)
+            time.sleep(0.5)
+        else:
+            print("invalid command")
+        # usb.connection.write(msg)
 main()
+
+# print(str.encode("5"))
